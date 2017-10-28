096567ef2afa8c9a8c103c82a9474a8e168aa4ce
Pushkar Khedekar <pushkar@Pushkars-MacBook-Pro.local>
2017-10-28 16:17:21 -0400

    wList v.5 - introducing tabbed views in app

---
+++ WishListApp2/WishListApp2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WishListApp2
+//
+//  Created by Pushkar Khedekar on 10/28/17.
+//  Copyright © 2017 Pushkar Khedekar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
